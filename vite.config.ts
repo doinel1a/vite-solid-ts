21a2a25cffb1723ef88785fadafff54190fdd116
doinel1a <doinel1atanasiu@gmail.com>
2023-04-13 14:47:27 +0200

    refactor: dev server host & port

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,17 @@ import { partytownVite } from '@builder.io/partytown/utils';
 import solidPlugin from 'vite-plugin-solid';
 import { defineConfig } from 'vite';
 
+import _config from './_config';
+
+const HOST = _config.server.host;
+const PORT = _config.server.port;
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: HOST,
+    port: PORT
+  },
   plugins: [
     solidPlugin(),
     partytownVite({
