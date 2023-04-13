3eb21d2331e56d318c6604d4a35d64a33e607f18
doinel1a <doinel1atanasiu@gmail.com>
2023-04-13 14:45:17 +0200

    refactor: header

--- src/app.tsx
+++ src/app.tsx
@@ -1,15 +1,19 @@
 import Counter from './components/counter';
 import GithubCorner from './components/github-corner';
+import Header from './components/header';
 
 function App() {
   return (
-    <main class='flex h-screen flex-col items-center justify-center bg-primary text-color'>
-      <GithubCorner
-        title='Get started on GitHub'
-        url='https://github.com/doinel1a/vite-react-ts-starter'
-      />
-      <Counter />
-    </main>
+    <>
+      <Header />
+      <main class='flex h-screen flex-col items-center justify-center bg-primary text-color'>
+        <GithubCorner
+          title='Get started on GitHub'
+          url='https://github.com/doinel1a/vite-solid-ts'
+        />
+        <Counter />
+      </main>
+    </>
   );
 }
 
